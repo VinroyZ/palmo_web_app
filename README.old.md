2879e2d528629aa1d0e77fc2ef315495eb4419c3
mangev01 <68163631+VinroyZ@users.noreply.github.com>
2023-05-17 02:53:40 -0400

    Hosting Site

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# PalmoWebApp
+A Dating Site for helping people get their foot out there again
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Palmo Coming soon!</h1>
+        <h2>Logic made by Ji Young Lee</h2>
+        <h2>Developed by Elijah Mangal</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
